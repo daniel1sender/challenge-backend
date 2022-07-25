39230241b5383faebdd633b6e1056c956c0c1fa7
Daniel Sender <daniel.saul7@gmail.com>
2022-07-25 17:05:17 -0300

    feat: add repository pattern to link implementation

---
+++ domain/repository.go
@@ -0,0 +1,17 @@
+package domain
+
+import (
+	"context"
+
+	"github.com/daniel1sender/alura-flix/domain/entity"
+)
+
+type Repository interface {
+	GetAll(ctx context.Context) ([]entity.Video, error)
+	Insert(ctx context.Context, video entity.Video) error
+	GetByID(ctx context.Context, id string) (entity.Video, error)
+	DeleteById(ctx context.Context, id string) error
+	UpdateTitleByID(ctx context.Context, id, title string) error
+	UpdateURLByID(ctx context.Context, id, url string) error
+	UpdateDescriptionByID(ctx context.Context, id, description string) error
+}
