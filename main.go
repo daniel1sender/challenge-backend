3d7e11b7fdae2cde1570475f89e31518392088b6
Daniel Sender <daniel.saul7@gmail.com>
2022-07-25 16:59:12 -0300

    feat: add main func to run aplication

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/daniel1sender/alura-flix/domain/usecases"
+	"github.com/daniel1sender/alura-flix/gateways"
+	handler "github.com/daniel1sender/alura-flix/gateways/http"
+	"github.com/daniel1sender/alura-flix/gateways/postgres"
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v4/pgxpool"
+)
+
+func main() {
+	ctx := context.Background()
+	gateways.RunMigrations()
+	conn, _ := pgxpool.Connect(ctx, "postgres://postgres:1234@localhost:5432/challenge?sslmode=disable")
+
+	flixStorage := postgres.NewStorage(conn)
+	flixUseCase := usecases.NewUseCase(flixStorage)
+	handler := handler.NewHandler(flixUseCase)
+
+	r := gin.Default()
+	r.GET("/videos", handler.GetAll)
+	r.GET("/videos/:id", handler.GetByID)
+	r.POST("/videos", handler.Create)
+	r.DELETE("/videos/:id", handler.DeleteByID)
+	r.PATCH("/videos/:id", handler.UpdateByID)
+
+	server := &http.Server{
+		Handler:      r,
+		Addr:         ":8000",
+		WriteTimeout: 30 * time.Second,
+		ReadTimeout:  30 * time.Second,
+	}
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatalf("failed to listen and serve due to: %s", err)
+	}
+}
