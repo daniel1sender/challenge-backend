eea6b06354d3cfe8f5c093c4d3d7fca6a3febe40
Daniel Sender <daniel.saul7@gmail.com>
2022-07-25 17:02:25 -0300

    feat: add function to run migrations

---
+++ gateways/migrations.go
@@ -0,0 +1,20 @@
+package gateways
+
+import (
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+)
+
+func RunMigrations() error {
+	m, err := migrate.New(
+		"file://gateways/migrations",
+		"postgres://postgres:1234@localhost:5432/challenge?sslmode=disable")
+	if err != nil {
+		return err
+	}
+	if err := m.Up(); err != nil {
+		return err
+	}
+	return nil
+}
